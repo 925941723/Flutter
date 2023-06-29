c1d666261a291029896f5b3640e3ea1d769cbb60
ZengHaiTao <925941723@qq.com>
2023-06-29 10:41:43 +0800

    实体类json序列化，使用方式总结

--- flutter_application/lib/page/test/model2json/model_to_json_page.dart
+++ flutter_application/lib/page/test/model2json/model_to_json_page.dart
@@ -21,20 +21,22 @@ class ModelToJsonPage extends StatelessWidget {
   Widget build(BuildContext context) {
     // JSON和序列化数据  官方方式 https://flutter.cn/docs/data-and-backend/json
     // 缺点：会生成额外的文件
-    // 推荐使用这种方式，因为实体类嵌套必不可少
     AccountModel accountModel = AccountModel('account', 'password');
     UserModel userModel = UserModel('name', accountModel);
     // print('userModel:${userModel.toJson()}');
     // 使用网页转换方式 https://app.quicktype.io/
-    // 缺点：嵌套的时候需要自己写代码
+    // 缺点：嵌套的时候需要手动建多个文件，只有在生成非空的实体类时比较好用
     AccountModel2 accountModel2 = AccountModel2(account: 'account', password: 'password');
     UserModel2 userModel2 = UserModel2(name: 'name',accountModel2: accountModel2);
     // print('userModel2:${userModel2.toJson()}');
     // 使用Plugins安装JsonToDart，右键models文件夹，选new，选JsonToDart。在弹窗的setting勾上null-safety，其他全不勾。然后输入json字符串，输入名字，点生成
-    // 缺点：嵌套的时候需要自己写代码；必须生成空安全的，否则需要手动加入required
+    // 缺点：只有在生成空安全(即可以为空)的实体类比较好用
     AccountModel3 accountModel3 = AccountModel3(account: 'account', password: 'password');
     UserModel3 userModel3 = UserModel3(name: 'name',accountModel3: accountModel3);
     // print('userModel3:${userModel3.toJson()}');
+    /// 推荐:
+    /// 本地实体使用第2种方式
+    /// 请求返回使用第3种方式，对于不可空的属性设置成required，并去掉问号、加入late
 
     return Scaffold(
       appBar: AppBar(
